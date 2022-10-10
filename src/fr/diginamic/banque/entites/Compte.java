803d6ad7b4027d88d3826cd3e50c87f7cc281f23
DraphaelT <115068828+DraphaelT@users.noreply.github.com>
2022-10-10 16:36:24 +0200

    TP5

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,29 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	
+	private int numCompte;
+	private int soldeCompte;
+	
+	Compte(int numCompte, int soldeCompte){
+		this.numCompte = numCompte;
+		this.soldeCompte = soldeCompte;
+	}
+
+	public int getNumCompte() {
+		return numCompte;
+	}
+
+	void setNumCompte(int numCompte) {
+		this.numCompte = numCompte;
+	}
+
+	public double getSoldeCompte() {
+		return soldeCompte;
+	}
+
+	void setSoldeCompte(int soldeCompte) {
+		this.soldeCompte = soldeCompte;
+	}
+	
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,15 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Compte compte = new Compte(45,10000);
+		
+		compte.getNumCompte();
+		compte.getSoldeCompte();
+		
+		System.out.println("Numero de compte: "+compte.getNumCompte()+" Votre Solde: "+compte.getSoldeCompte());
+	}
+
+}
