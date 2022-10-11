5b9b13375f572d8d344d69950b88221d681aa52e
DraphaelT <115068828+DraphaelT@users.noreply.github.com>
2022-10-11 10:16:03 +0200

    TP7

---
+++ src/fr/diginamic/banque/entites/CompteTaux.java
@@ -0,0 +1,15 @@
+package fr.diginamic.banque.entites;
+
+
+public class CompteTaux extends Compte{	
+	private int tauxRemu;
+	
+	CompteTaux(int numCompte, int soldeCompte, int tauxRemu) {
+		super(numCompte, soldeCompte);
+		this.tauxRemu = tauxRemu;
+	}
+	public String toString(){
+		String idCompte = super.toString();
+	    return idCompte +" Le taux de Remuneration est :"+tauxRemu;
+	  }
+}
--- src/fr/diginamic/banque/entites/TestBanque.java
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -4,6 +4,7 @@ public class TestBanque {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Compte compte = new Compte(45,10000);
 			
 		compte.getNumCompte();
@@ -12,6 +13,20 @@ public class TestBanque {
 		System.out.println(compte.toString());
 		
 		System.out.println("Numero de compte: "+compte.getNumCompte()+" Votre Solde: "+compte.getSoldeCompte());
+		
+		//TP7
+		
+		Compte[] tabCompte = new Compte[2];
+		
+		Compte compteG = new Compte(85,95420);
+		CompteTaux compteP = new CompteTaux(46,250000,850);
+		
+		tabCompte[0]=compteG;
+		tabCompte[1]=compteP;
+		
+		for(int loop=1;loop<=tabCompte.length;loop++) {
+			System.out.println(tabCompte[loop-1].toString());
+		}
 	}
 
 }
